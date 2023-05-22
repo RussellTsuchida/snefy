galaxy example and fixed bug in units of log likelihood for VMF

squared_neural_families/nets/integrate.py
@@ -76,9 +76,9 @@ class SquaredNN(torch.nn.Module):
         elif (measure == 'uniformsphere'):
             # Reciprocal of surface area of sphere
             self.base_measure = None
-            # This doesn't affect the fit anyway
+            # This doesn't affect the fit anyway. Affects units of log likelihood
             self.pdf_ = lambda x, log_scale:\
-                -self.d/2 * np.log(2*np.pi) + \
+                -self.d/2 * np.log(np.pi) - np.log(2) + \
                 torch.lgamma(torch.tensor(self.d/2)) if log_scale \
                 else torch.exp(torch.lgamma(torch.tensor(self.d/2)))/\
                     (2*np.pi**(self.d/2))

squared_neural_families/nets/kernels.py
@@ -151,7 +151,4 @@ def vmf_kernel(W1, W2, B1, B2):
     fac = (torch.exp(sum_) - torch.exp(-sum_))/sum_
     fac[fac!=fac] = 2
 
-    return 2*np.pi*fac*exp_fac
-
-
-
+    return fac*exp_fac/2